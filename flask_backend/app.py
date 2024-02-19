30a566c37c854fd59c02577528aca61892d92fb6
Tony Tian <tonytian@utexas.edu>
2024-02-19 15:57:09 -0600

    amended format in about us

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -15,6 +15,8 @@ def login():
 def products():
     return render_template("products.html")
 
-
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True)
